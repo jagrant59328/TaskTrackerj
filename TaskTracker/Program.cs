0bfceecb2336f646779d16f5aae088476be17689
jagrant59328 <jagrant59328@gmail.com>
2026-01-27 17:22:32 -0800

    Add do While Loop

--- TaskTracker/Program.cs
+++ TaskTracker/Program.cs
@@ -1,14 +1,25 @@
-
-Console.WriteLine("Welcome to TaskTracker!");
-Console.WriteLine("1. List Tasks");
-Console.WriteLine("2. About");
-Console.Write("Choose an option: ");
-
-var input = Console.ReadLine();
-
-if (input == "2")
-{
-    Console.WriteLine("Jayden");
-    Console.WriteLine("jagrant59328");
-    Console.WriteLine(DateTime.Today);
-}
+var shouldContinue = true;
+do {
+    Console.WriteLine("Welcome to TaskTracker!");
+    Console.WriteLine("1. List Tasks");
+    Console.WriteLine("2. About");
+    Console.WriteLine("3. Exit");
+    Console.Write("Choose an option: ");
+ 
+    int.TryParse(Console.ReadLine(), null, out int input);
+ 
+    if (input == 2)
+    {
+        Console.WriteLine("Jayden Grant");
+        Console.WriteLine("Jagrant59328");
+        Console.WriteLine(DateTime.IsLeapYear(2007));
+        Console.WriteLine(DateTime.Now);
+    }
+ 
+    if(input == 3)
+    {
+        shouldContinue = false;
+    }
+ 
+    // make it so that the program does't close after an action is done
+} while (shouldContinue);
